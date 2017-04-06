a9a8f1e5a84a309e79de877482c3d4784f23acfa
Oleg Katkov <lezh1k.vohrer@gmail.com>
2017-04-06 11:17:34 +0600

    this compiller is totally unhappy. doesn't understand me :-D

--- include/stun.h
+++ include/stun.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 int32_t stun_prepare_message(int32_t n, char* msg, struct sockaddr* addr, int32_t* change_request);
-int32_t stun_handle_change_addr(struct sockaddr* changed_addr, sockaddr *source_addr, void* pool);
+int32_t stun_handle_change_addr(struct sockaddr* changed_addr, struct sockaddr *source_addr, void* pool);
 
 #endif // STUN_H
